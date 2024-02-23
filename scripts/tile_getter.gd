0c177e9adebbe7123e64dde219e4305bd3aee7f6
muriplz <46825658+muriplz@users.noreply.github.com>
2024-02-23 07:02:20 +0100

    more work!

--- scripts/tile_getter.gd
+++ scripts/tile_getter.gd
@@ -3,27 +3,24 @@ extends Node
 signal image_loaded(texture, tile_coords)
 
 var active_requests = {}
-var max_concurrent_requests = 1  # Limit the number of concurrent requests
+var max_concurrent_requests = 1
 
 func queue_tile(tile_coords: Vector2):
-	# Limit the number of active requests
 	if active_requests.size() >= max_concurrent_requests:
-		# Could queue the request or handle backpressure differently
 		return
-
-	var url = "https://kryeit.com/images/tiles/15/%s_%s.png" % [tile_coords.x, tile_coords.y]
+	var url = "https://kryeit.com/images/tiles/17/%s_%s.png" % [tile_coords.x, tile_coords.y]
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
 	http_request.request_completed.connect(self._http_request_completed)
 	http_request.request(url)
 	set_meta("tile_coords", tile_coords)
-	active_requests[tile_coords] = http_request  # Track the request
+	active_requests[tile_coords] = http_request
 
 func _http_request_completed(status: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
 	var tile_coords = get_meta("tile_coords")
 	if active_requests.has(tile_coords):
-		active_requests[tile_coords].queue_free()  # Clean up the HTTPRequest node
-		active_requests.erase(tile_coords)  # Remove from active requests
+		active_requests[tile_coords].queue_free()
+		active_requests.erase(tile_coords)
 
 	if status == OK and response_code == 200:
 		var image = Image.new()
