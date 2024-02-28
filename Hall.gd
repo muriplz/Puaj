5fbfc1b7861ecf17d9de57b1f8e4b76ea5aa9ac1
muriplz <46825658+muriplz@users.noreply.github.com>
2024-02-29 00:41:52 +0100

    w

---
+++ Hall.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+func _ready():
+	var margin_value = 100
+	add_theme_constant_override("margin_top", margin_value)
+	add_theme_constant_override("margin_left", margin_value)
+	add_theme_constant_override("margin_bottom", margin_value)
+	add_theme_constant_override("margin_right", margin_value)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/UIOpener.gd
+++ scripts/UIOpener.gd
@@ -6,10 +6,25 @@ var hall
 func _ready():
 	# Find the Hall panel container node
 	hall = get_node("../Hall")
-
+	hall.visible = false
 	# Connect the button's "pressed" signal to the _on_BurguerOpener_pressed() method
 	connect("pressed", Callable(self, "_on_BurguerOpener_pressed"))
 
 func _on_BurguerOpener_pressed():
-	# Toggle the visibility of the Hall panel container
-	hall.visible = not hall.visible
+
+	# Toggle visibility: Hide the Button, show the Hall
+	self.visible = false
+	hall.visible = true
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var click_pos = hall.get_local_mouse_position()
+		
+		# Check if the click is outside the Hall
+		if not hall.get_rect().has_point(click_pos):
+			# If the Hall is visible and the click is outside, toggle the visibility
+			if hall.visible:
+				hall.visible = false
+				self.visible = true
+
+		# Ensure the input doesn't propagate further if it's handled
