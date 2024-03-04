fc383380d2315a33ec54b7b72fecd7e718c353b6
muriplz <46825658+muriplz@users.noreply.github.com>
2024-03-04 13:16:35 +0100

    some ui

--- Label.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-func _process(delta: float) -> void:
-	var fps = Engine.get_frames_per_second()
-	text = "FPS: %d" % fps
--- scripts/UIOpener.gd
+++ scripts/UIOpener.gd
@@ -1,30 +1,12 @@
 extends Button
 
 # Reference to the panel container
-var hall
+@onready var ui = $"../UI"
 
 func _ready():
 	# Find the Hall panel container node
-	hall = $"../PanelContainer"
-	hall.visible = false
-	# Connect the button's "pressed" signal to the _on_BurguerOpener_pressed() method
-	connect("pressed", Callable(self, "_on_BurguerOpener_pressed"))
+	ui.visible = false
+	connect("pressed", Callable(self, "_on_pressed"))
 
-func _on_BurguerOpener_pressed():
-
-	# Toggle visibility: Hide the Button, show the Hall
-	self.visible = false
-	hall.visible = true
-
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		var click_pos = hall.get_local_mouse_position()
-		
-		# Check if the click is outside the Hall
-		if not hall.get_rect().has_point(click_pos):
-			# If the Hall is visible and the click is outside, toggle the visibility
-			if hall.visible:
-				hall.visible = false
-				self.visible = true
-
-		# Ensure the input doesn't propagate further if it's handled
+func _on_pressed():
+	ui.visible = !ui.visible
